c029d9b08c99f92b20638b037f1f030a4ae9402e
VitorFaria <vitorfaria017@outlook.com>
2019-03-06 20:22:21 -0300

    melhoria na adição de disciplinas

--- frontend/src/app/disciplina/add-disciplina/add-disciplina.component.ts
+++ frontend/src/app/disciplina/add-disciplina/add-disciplina.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 import {ApiService} from "../../core/api.service";
+import {Curso} from "../../model/curso.model";
 
 @Component({
   selector: 'app-add-disciplina',
@@ -13,6 +14,7 @@ export class AddDisciplinaComponent implements OnInit {
   constructor(private formBuilder: FormBuilder,private router: Router, private apiService: ApiService) { }
 
   addForm: FormGroup;
+  cursos: Curso[];
   submitted = false;
   errorText = null;
 
@@ -25,8 +27,15 @@ export class AddDisciplinaComponent implements OnInit {
       nome: ['', Validators.required],
       id_curso: ['', Validators.required]
     });
-
+    this.getCursos();
   }
+
+  getCursos(): void {
+    this.apiService.getCursos()
+      .subscribe( data => {
+          this.cursos = data.result;
+      });
+  };
 
   onSubmit() {
     this.submitted = true;
